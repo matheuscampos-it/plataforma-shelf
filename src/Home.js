191318b609c8a333c8e93c96bffe36cf17f65839
Matheus <campos98matheus@gmail.com>
2023-06-22 15:07:39 -0300

    Botão registrar e correções no package.json

--- src/Home.js
+++ src/Home.js
@@ -13,21 +13,6 @@ function Home() {
   const [salario, setSalario] = useState(0);
   const [investimentos, setInvestimentos] = useState(0);
   const [receitasOutros, setReceitasOutros] = useState(0);
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // Estado de login do usuário
-
-  if (!isLoggedIn) {
-    return (
-      <main>
-        <div className="generic-home">
-          <img src={logo} alt="Logo" />
-          <h1>Bem-vindo ao Shelf</h1>
-          <p>O Shelf é um projeto de controle de finanças pessoais.</p>
-          <p>Faça o login para acessar sua conta.</p>
-          <Link to="/login">Login</Link>
-        </div>
-      </main>
-    );
-  }
 
   const despesasTotal = aluguel + alimentacao + transporte + despesasOutros;
   const receitaTotal = salario + investimentos + receitasOutros;
@@ -64,126 +49,141 @@ function Home() {
     saldoBorderColor = '#539165'; // Verde
   }
 
+  const handleRegistrarMovimentacao = () => {
+    // Lógica para registrar a movimentação
+    // Implemente de acordo com sua necessidade
+    console.log('Movimentação registrada!');
+  };
+
   return (
     <main>
-      <div className='shelf'>
-        <section id='shelf section-content'>
-          <div className='coluna-esq'>
+      <div className="shelf">
+        <section className="section-content">
+          <div className="coluna-esq">
             <h2>Saldo Atual</h2>
-            <input
-              className='input-box'
-              type='number'
-              value={saldoAtual}
-              onChange={(event) => setSaldoAtual(Number(event.target.value))}
-            />
+            <p>R$ {saldoAtual.toFixed(2)}</p>
           </div>
-          <div className='coluna-dir'></div>
+          <div className="coluna-dir"></div>
         </section>
 
-        <section className='despesas section-content' id='shelf'>
-          <div className='coluna-esq'>
+        <section className="despesas section-content">
+          <div className="coluna-esq">
             <h2 onClick={() => alternarDropdown('despesas')}>Despesas do Mês</h2>
             {despesasAberto ? (
-              <>
-                <div className='input-group'>
+              <div className="detalhes">
+                <div className="input-group">
                   <label>Aluguel:</label>
                   <input
-                    className='input-box'
-                    type='number'
+                    className="input-box"
+                    type="number"
                     value={aluguel}
                     onChange={(event) => setAluguel(Number(event.target.value))}
                   />
                 </div>
-                <div className='input-group'>
+                <div className="input-group">
                   <label>Alimentação:</label>
                   <input
-                    className='input-box'
-                    type='number'
+                    className="input-box"
+                    type="number"
                     value={alimentacao}
                     onChange={(event) => setAlimentacao(Number(event.target.value))}
                   />
                 </div>
-                <div className='input-group'>
+                <div className="input-group">
                   <label>Transporte:</label>
                   <input
-                    className='input-box'
-                    type='number'
+                    className="input-box"
+                    type="number"
                     value={transporte}
                     onChange={(event) => setTransporte(Number(event.target.value))}
                   />
                 </div>
-                <div className='input-group'>
+                <div className="input-group">
                   <label>Outros:</label>
                   <input
-                    className='input-box'
-                    type='number'
+                    className="input-box"
+                    type="number"
                     value={despesasOutros}
                     onChange={(event) => setDespesasOutros(Number(event.target.value))}
                   />
                 </div>
-              </>
+                <button className="registrar-btn" onClick={handleRegistrarMovimentacao}>
+                  Registrar Movimentação
+                </button>
+              </div>
             ) : (
-              <p>R${despesasTotal}</p>
+              <p>R$ {despesasTotal.toFixed(2)}</p>
             )}
           </div>
-          <div className='coluna-dir'></div>
+          <div className="coluna-dir"></div>
         </section>
 
-        <section className='receita section-content' id='shelf'>
-          <div className='coluna-esq'>
+        <section className="receita section-content">
+          <div className="coluna-esq">
             <h2 onClick={() => alternarDropdown('receitas')}>Receita Esperada</h2>
             {receitasAberto ? (
-              <>
-                <div className='input-group'>
+              <div className="detalhes">
+                <div className="input-group">
                   <label>Salário:</label>
                   <input
-                    className='input-box'
-                    type='number'
+                    className="input-box"
+                    type="number"
                     value={salario}
                     onChange={(event) => setSalario(Number(event.target.value))}
                   />
                 </div>
-                <div className='input-group'>
+                <div className="input-group">
                   <label>Investimentos:</label>
                   <input
-                    className='input-box'
-                    type='number'
+                    className="input-box"
+                    type="number"
                     value={investimentos}
                     onChange={(event) => setInvestimentos(Number(event.target.value))}
                   />
                 </div>
-                <div className='input-group'>
+                <div className="input-group">
                   <label>Outros:</label>
                   <input
-                    className='input-box'
-                    type='number'
+                    className="input-box"
+                    type="number"
                     value={receitasOutros}
                     onChange={(event) => setReceitasOutros(Number(event.target.value))}
                   />
                 </div>
-              </>
+                <button className="registrar-btn" onClick={handleRegistrarMovimentacao}>
+                  Registrar Movimentação
+                </button>
+              </div>
             ) : (
-              <p>R${receitaTotal}</p>
+              <p>R$ {receitaTotal.toFixed(2)}</p>
             )}
           </div>
-          <div className='coluna-dir'></div>
+          <div className="coluna-dir"></div>
         </section>
 
-        <section className='saldo-final' id='shelf' style={{ border: `1px solid ${saldoBorderColor}`, boxShadow: `0 2px 4px rgba(0, 0, 0, 0.1)` }}>
-          <div className='coluna-esq'>
+        <section
+          className="saldo-final"
+          style={{ border: `1px solid ${saldoBorderColor}`, boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}
+        >
+          <div className="coluna-esq">
             <h2>Saldo ao final do mês</h2>
             <p style={{ color: saldoFinal < 0 ? '#FC3503' : saldoFinal <= 200 ? '#F7C04A' : '#539165' }}>
-              R${saldoFinal}
+              R$ {saldoFinal.toFixed(2)}
             </p>
           </div>
-          <div className='coluna-dir'></div>
+          <div className="coluna-dir"></div>
         </section>
 
         <section className={`mensagem ${saldoFinal < 0 ? 'negativo' : saldoFinal <= 200 ? 'atencao' : 'positivo'}`}>
-          {saldoFinal < 0 && <p>Cuidado! Sua conta está no vermelho, procure economizar! Nada de fast food pelas próximas semanas.</p>}
-          {saldoFinal >= 0 && saldoFinal <= 200 && <p>Atenção, você ainda não está no vermelho, mas está quase!</p>}
-          {saldoFinal > 200 && <p>Legal, tudo tranquilo, compre um açaí para comemorar!</p>}
+          {saldoFinal < 0 && (
+            <p>Cuidado! Seu saldo está negativo. Considere revisar suas despesas e buscar novas receitas.</p>
+          )}
+          {saldoFinal <= 200 && saldoFinal >= 0 && (
+            <p>Atenção! Seu saldo está baixo. Faça um planejamento financeiro para evitar problemas.</p>
+          )}
+          {saldoFinal > 200 && <p>Parabéns! Seu saldo está positivo. Continue mantendo o controle financeiro.</p>}
         </section>
+
       </div>
     </main>
   );
